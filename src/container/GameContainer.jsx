0dfc45b15705dccc0d81734a43b309c72b4c5e34
ben-ghirardani <pretty.fiendish@gmail.com>
2017-10-01 12:49:42 +0100

    initial setup

---
+++ src/container/GameContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GameBoard from './component/GameBoard';
+import Card from './component/Card';
+
+class GameContainer extends React.Component {
+
+} 
+
+export default GameContainer;
